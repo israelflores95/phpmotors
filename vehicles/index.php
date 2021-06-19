3ec5671c087d2ec50dafad9799ca8870199213de
Israel Flores <israelflores@byui.edu>
2021-06-19 12:39:34 -0600

    activity 3

--- vehicles/index.php
+++ vehicles/index.php
@@ -55,61 +55,56 @@ $action = filter_input(INPUT_POST, 'action');
 
     case 'add-vehicle';
 
-    // get info from html form and into php variables
-    $invMake = TRIM(filter_input(INPUT_POST, 'invMake', FILTER_SANITIZE_STRING));
-    $invModel = TRIM(filter_input(INPUT_POST, 'invModel', FILTER_SANITIZE_STRING));
-    $invDescription = TRIM(filter_input(INPUT_POST, 'invDescription', FILTER_SANITIZE_STRING));
-    $invImage = TRIM(filter_input(INPUT_POST, 'invImage', FILTER_SANITIZE_STRING));
-    $invThumbnail = TRIM(filter_input(INPUT_POST, 'invThumbnail', FILTER_SANITIZE_STRING));
-    $invPrice = TRIM(filter_input(INPUT_POST, 'invPrice', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION));
-    $invStock = TRIM(filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_FLOAT));
-    $invColor = TRIM(filter_input(INPUT_POST, 'invColor', FILTER_SANITIZE_STRING));
-    $classificationId = TRIM(filter_input(INPUT_POST, 'classificationId', FILTER_SANITIZE_NUMBER_INT));
+        // get info from html form and into php variables
+        $invMake = TRIM(filter_input(INPUT_POST, 'invMake', FILTER_SANITIZE_STRING));
+        $invModel = TRIM(filter_input(INPUT_POST, 'invModel', FILTER_SANITIZE_STRING));
+        $invDescription = TRIM(filter_input(INPUT_POST, 'invDescription', FILTER_SANITIZE_STRING));
+        $invImage = TRIM(filter_input(INPUT_POST, 'invImage', FILTER_SANITIZE_STRING));
+        $invThumbnail = TRIM(filter_input(INPUT_POST, 'invThumbnail', FILTER_SANITIZE_STRING));
+        $invPrice = TRIM(filter_input(INPUT_POST, 'invPrice', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION));
+        $invStock = TRIM(filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_FLOAT));
+        $invColor = TRIM(filter_input(INPUT_POST, 'invColor', FILTER_SANITIZE_STRING));
+        $classificationId = TRIM(filter_input(INPUT_POST, 'classificationId', FILTER_SANITIZE_NUMBER_INT));
 
-//    var_dump($invImage);
-//    exit;
+        // check for empty fields and report message
+        if(empty($invMake) || empty($invModel) || empty($invDescription) || empty($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invColor) || empty($classificationId)){
+            $message = '<p class = "server-valadation">Please provide information for all empty form fields.</p>';
+            // include '../view/add-vehicle.php';
+            header('Location: /phpmotors/view/add-vehicle.php');
+            exit;
+        }
 
-    // check for empty fields and report message
-    if(empty($invMake) || empty($invModel) || empty($invDescription) || empty($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invColor) || empty($classificationId)){
-        $message = '<p class = "server-valadation">Please provide information for all empty form fields.</p>';
-        // include '../view/add-vehicle.php';
-        header('Location: /phpmotors/view/add-vehicle.php');
-        exit;
-    }
-
-    // send data to model
-    $addVehicleOutcome = addVehicle ($invMake, $invModel, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invColor, $classificationId);
-    
-    if($addVehicleOutcome === 1){
-        $message = "<p>Vehicle added</p>";
-        include '../view/vehicle-management.php';
-        exit;
-    } else {
-        $message = "<p>Adding vehicle failed! please try again.</p>";
-        header('Location: /phpmotors/view/add-vehicle.php');
-        exit;
-    }
+        // send data to model
+        $addVehicleOutcome = addVehicle ($invMake, $invModel, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invColor, $classificationId);
+        
+        if($addVehicleOutcome === 1){
+            $message = "<p>Vehicle added</p>";
+            include '../view/vehicle-management.php';
+            exit;
+        } else {
+            $message = "<p>Adding vehicle failed! please try again.</p>";
+            header('Location: /phpmotors/view/add-vehicle.php');
+            exit;
+        }
     break;
-
-
 
     case 'add-classification';
 
-    // get info from html form and into php variables
-    $classificationName = filter_input(INPUT_POST, 'classificationName');
+        // get info from html form and into php variables
+        $classificationName = filter_input(INPUT_POST, 'classificationName');
 
-    // check for empty fields and report message
-    if(empty($classificationName)){
-        $message = '<p class = "server-valadation">*Please provide information for the empty field.</p>';
-        header('Location: /phpmotors/vehicles/index.php');
-        exit;
-    }
+        // check for empty fields and report message
+        if(empty($classificationName)){
+            $message = '<p class = "server-valadation">*Please provide information for the empty field.</p>';
+            header('Location: /phpmotors/vehicles/index.php');
+            exit;
+        }
 
-    // send data to model
-    $addClassificationOutcome = addClassification ($classificationName);
+        // send data to model
+        $addClassificationOutcome = addClassification ($classificationName);
 
-        // include '../view/vehicle-management.php';
-        header('Location: /phpmotors/vehicles/index.php');
+            // include '../view/vehicle-management.php';
+            header('Location: /phpmotors/vehicles/index.php');
     break;
 
     case 'getInventoryItems': 
@@ -119,7 +114,7 @@ $action = filter_input(INPUT_POST, 'action');
         $inventoryArray = getInventoryByClassification($classificationId); 
         // Convert the array to a JSON object and send it back 
         echo json_encode($inventoryArray); 
-        break;
+    break;
 
     case 'mod':
         $invId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
@@ -130,49 +125,81 @@ $action = filter_input(INPUT_POST, 'action');
            }
 
         include '../view/vehicle-update.php';
+        exit;
     break;
 
     case 'updateVehicle':
         // get info from html form and into php variables
-    $invMake = TRIM(filter_input(INPUT_POST, 'invMake', FILTER_SANITIZE_STRING));
-    $invModel = TRIM(filter_input(INPUT_POST, 'invModel', FILTER_SANITIZE_STRING));
-    $invDescription = TRIM(filter_input(INPUT_POST, 'invDescription', FILTER_SANITIZE_STRING));
-    $invImage = TRIM(filter_input(INPUT_POST, 'invImage', FILTER_SANITIZE_STRING));
-    $invThumbnail = TRIM(filter_input(INPUT_POST, 'invThumbnail', FILTER_SANITIZE_STRING));
-    $invPrice = TRIM(filter_input(INPUT_POST, 'invPrice', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION));
-    $invStock = TRIM(filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_FLOAT));
-    $invColor = TRIM(filter_input(INPUT_POST, 'invColor', FILTER_SANITIZE_STRING));
-    $classificationId = TRIM(filter_input(INPUT_POST, 'classificationId', FILTER_SANITIZE_NUMBER_INT));
+        $invMake = TRIM(filter_input(INPUT_POST, 'invMake', FILTER_SANITIZE_STRING));
+        $invModel = TRIM(filter_input(INPUT_POST, 'invModel', FILTER_SANITIZE_STRING));
+        $invDescription = TRIM(filter_input(INPUT_POST, 'invDescription', FILTER_SANITIZE_STRING));
+        $invImage = TRIM(filter_input(INPUT_POST, 'invImage', FILTER_SANITIZE_STRING));
+        $invThumbnail = TRIM(filter_input(INPUT_POST, 'invThumbnail', FILTER_SANITIZE_STRING));
+        $invPrice = TRIM(filter_input(INPUT_POST, 'invPrice', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION));
+        $invStock = TRIM(filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_FLOAT));
+        $invColor = TRIM(filter_input(INPUT_POST, 'invColor', FILTER_SANITIZE_STRING));
+        $classificationId = TRIM(filter_input(INPUT_POST, 'classificationId', FILTER_SANITIZE_NUMBER_INT));
 
-    $invId = filter_input(INPUT_POST, 'invId', FILTER_SANITIZE_NUMBER_INT);
+        $invId = filter_input(INPUT_POST, 'invId', FILTER_SANITIZE_NUMBER_INT);
 
-//    var_dump($invImage);
-//    exit;
+        // check for empty fields and report message
+        if(empty($invMake) || empty($invModel) || empty($invDescription) || empty($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invColor) || empty($classificationId)){
+            $message = '<p class = "server-valadation">Please provide information for all empty form fields.</p>';
+            // include '../view/add-vehicle.php';
+            header('Location: /phpmotors/view/vehicle-update.php');
+            exit;
+        }
 
-    // check for empty fields and report message
-    if(empty($invMake) || empty($invModel) || empty($invDescription) || empty($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invColor) || empty($classificationId)){
-        $message = '<p class = "server-valadation">Please provide information for all empty form fields.</p>';
-        // include '../view/add-vehicle.php';
-        header('Location: /phpmotors/view/vehicle-update.php');
-        exit;
-    }
-
-    // send data to model
-    $updateResult = updateVehicle($invMake, $invModel, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invColor, $classificationId, $invId);
+        // send data to model
+        $updateResult = updateVehicle($invMake, $invModel, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invColor, $classificationId, $invId);
+        
+        if($updateResult){
+            $message = "<p>Vehicle updated</p>";
+            $_SESSION['message'] = $message;
+            header( 'location: /phpmotors/vehicles/');
+            exit;
+        } else {
+            $message = "<p>Update vehicle failed! please try again.</p>";
+            header('Location: /phpmotors/view/vehicle-update.php');
+            exit;
+        }
     
-    if($updateResult){
-        $message = "<p>Vehicle updated</p>";
-        $_SESSION['message'] = $message;
-        header( 'location: /phpmotors/vehicles/');
+    break;    
+    
+    case 'del':
+        $invId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+        $invInfo = getInvItemInfo($invId);
+
+        if(count($invInfo)<1){
+            $message = 'Sorry, no vehicle information could be found.';
+           }
+
+        include '../view/vehicle-delete.php';
         exit;
-    } else {
-        $message = "<p>Update vehicle failed! please try again.</p>";
-        header('Location: /phpmotors/view/vehicle-update.php');
-        exit;
-    }
+    break;
+    
+    case 'deleteVehicle':
+         // get info from html form and into php variables
+         $invMake = TRIM(filter_input(INPUT_POST, 'invMake', FILTER_SANITIZE_STRING));
+         $invModel = TRIM(filter_input(INPUT_POST, 'invModel', FILTER_SANITIZE_STRING));
+         $invId = filter_input(INPUT_POST, 'invId', FILTER_SANITIZE_NUMBER_INT);
+ 
+         // send data to model
+         $deleteResult = deleteVehicle($invId);
+         
+         if($deleteResult){
+             $message = "<p>Vehicle Deleted</p>";
+             $_SESSION['message'] = $message;
+             header( 'location: /phpmotors/vehicles/');
+             exit;
+         } else {
+             $message = "<p>Delete vehicle failed! please try again.</p>";
+             header('Location: /phpmotors/view/vehicle-update.php');
+             exit;
+         }
     break;
 
-    default:
+        default:
     $classificationList = buildClassificationList($classifications);
         include '../view/vehicle-management.php';
  };
---
+++ view/vehicle-delete.php
@@ -0,0 +1,62 @@
+<?php
+    if ($_SESSION['clientLevel'] < 2 && !$_SESSION['loggedin']) {
+        header('Location: ../index.php');
+    }
+  ?><!doctype html>
+<html lang="en">
+<head>
+
+  <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/head.php'; ?>
+  <title><?php if(isset($invInfo['invMake'])){ echo "Delete $invInfo[invMake] $invInfo[invModel]";} ?> | PHP Motors</title>
+
+</head>
+
+<body>
+  <section class="content">
+
+<!-- php banner snippet -->
+  <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/banner.php'; ?>
+
+  <!-- nav php snippet -->
+  <nav id="nav"><?php echo $navList; ?></nav>
+
+  <section>
+    <!-- headings for vehicles -->
+    <h1><?php if(isset($invInfo['invMake'])){ 
+	echo "Delete $invInfo[invMake] $invInfo[invModel]";} ?></h1>
+
+<p>Confirm Vehicle Deletion. The delete is permanent.</p>
+
+  <?php
+  if (isset($message)) {
+  echo $message;
+  }
+  ?>
+    <!-- start form -->
+    <form class="addVehicle" method="POST" action="/phpmotors/vehicles/index.php">
+    
+    <Label for="invMake">Make*</Label>
+      <input type="text" id="invMake" name="invMake" readonly <?php if(isset($invMake)){echo "value='$invMake'";} elseif(isset($invInfo['invMake'])) {echo "value='$invInfo[invMake]'"; } ?> >
+
+    <Label for="invModel">Model*</Label>
+      <input type="text" id="invModel" name="invModel" readonly <?php if(isset($invModel)){echo "value='$invModel'";} ?>>
+
+    <Label for="invDescription">Description</Label>
+      <textarea id="invDescription" name="invDescription" readonly ><?php if(isset($invDescription)){echo $invDescription;} elseif(isset($invInfo['invDescription'])) {echo $invInfo['invDescription']; } ?></textarea>
+
+    <input id="addVehicle" type="submit" value="Delete Vehicle">
+
+    <input type="hidden" name="action" value="deleteVehicle">
+    <input type="hidden" name="invId" value=" <?php if(isset($invInfo['invId'])){ echo $invInfo['invId'];} ?>">
+
+    </form>
+
+  </section>
+
+
+  <!-- footer php snippet -->
+  <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?> 
+   
+  </section>
+</body>
+</html>
